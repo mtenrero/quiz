1d3ed873c581d747619b8ee6e6bd44ac3588a192
Marcos <tenrero@aol.com>
2015-07-13 05:52:01 +0200

    Creados ejs de respuesta y pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+// GET /quizes/question
+exports.question = function(request, response) {
+    response.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+// GET /quizes/answer
+exports.answer = function(request, response) {
+    if (req.query.respuesta === 'Roma') {
+        response.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+        response.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+}
