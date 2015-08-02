fd47f7d8c51af3df07880e8f1aa935bc46c98708
Marcos <tenrero@aol.com>
2015-08-02 02:59:51 +0200

    Despliegue en Heroku

--- models/models.js
+++ models/models.js
@@ -1,13 +1,35 @@
 var path = require('path');
 var Sequelize = require('sequelize');
 
+// Postgres DATABASE_URL = portgres://admin:a73a621fa94d1ddcc76870cda04ec06ad8fe4cbf151cd33b3d69ccd2c7ea8794@10.251.97.108:5432/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 var sequelize = new Sequelize(null, null, null, {
 					dialect: "sqlite",
 					storage: "quiz.sqlite"
 				});
 
+// Cargar Modelo ORM
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, {
+    dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+});
 
 sequelize.sync().success(function(){
 	Quiz.count().success(function(count){
