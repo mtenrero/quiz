6d62f8eb34426dbd049d8dfcc5d61f2857adea18
Marcos <tenrero@aol.com>
2015-08-02 02:52:08 +0200

    SQLITE3 + Sequelize

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,31 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
-exports.question = function(request, response) {
-    response.render('quizes/question', {pregunta: 'Capital de Italia'});
-};
+exports.question = function(req, res) {
+
+	models.Quiz.findAll().success(function(quiz){
+		res.render("quizes/question", { pregunta: quiz[0].pregunta });
+	});
+}
 
 // GET /quizes/answer
-exports.answer = function(request, response) {
-    if (request.query.respuesta === 'Roma') {
-        response.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-        response.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }
+exports.answer = function(req, res) {
+
+	models.Quiz.findAll().success(function(quiz){
+
+		var respuestaUsuario = req.query.respuesta.toUpperCase(),
+			data;
+
+		if(respuestaUsuario === quiz[0].respuesta.toUpperCase()){
+			data = {respuesta: "Correcto!"};
+		}else{
+			data = {respuesta: "Incorrecto"};
+		}
+
+		res.render("quizes/answer", data);
+	});
+}
+
+exports.author = function(req, res){
+	res.render("author");
 }
