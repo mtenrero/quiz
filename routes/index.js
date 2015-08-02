b8f55c13c55276c58681275be8e6ccc6b2fad8fa
Marcos <tenrero@aol.com>
2015-08-02 18:55:30 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload
+router.param('quizId', quizController.load);  // autoload :quizId
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 router.get('/author', function(request, response) {
     response.render('author');
 });
