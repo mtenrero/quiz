d015d5aede9799ef947d77cb0e609b06ff134843
Marcos <tenrero@aol.com>
2015-08-15 19:06:56 +0200

    P2P Select + correccion stylesheet

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,22 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-    models.Quiz.findAll().then(
-        function(quizes) {
-            res.render('quizes/index.ejs', {quizes: quizes, errors: []});
-        }
-    ).catch(function(error){next(error)});
+    if (req.query.search) {
+        var search = '%'+req.query.search.replace(/ /, '%')+'%';
+        models.Quiz.findAll({where: ["pregunta like ?", search], order: ["pregunta"]}).then(
+            function(quizes) {
+                res.render('quizes/index.ejs', {quizes: quizes, search: req.query.search, errors: []});
+            }
+        ).catch(function(error) {next(error);})
+    } else {
+        models.Quiz.findAll().then(
+            function (quizes) {
+                res.render('quizes/index.ejs', {quizes: quizes, search: '', errors: []});
+            }
+        ).catch(function (error) {
+                next(error)
+            });
+    }
 };
 
 // GET /quizes/:id
