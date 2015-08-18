a01de387fb23abfd375a75882e5a6625dfba2808
Marcos <tenrero@aol.com>
2015-08-18 17:14:12 +0200

    Autologout

--- app.js
+++ app.js
@@ -80,5 +80,42 @@ app.use(function(err, req, res, next) {
     });
 });
 
+//Middleware sesion
+// Gestión de la sesión
+app.use(function(req, res, next) {
+    var oldTransStart, newTransStart, offsetTrans;
+
+    // Control sobre todas las rutas excepto "/login"
+    if (!req.path.match(/\/login/)) {
+        // Almacenar ruta previa a la acción "login" / "logout" para redirección
+        req.session.redir = req.path;
+
+        // Control de timeout de sesión activa (usuario autenticado)
+        if (req.session.user) {
+            oldTransStart = (req.session.lastTransStart || Date.now());
+            newTransStart = Date.now();
+            offsetTrans   = (newTransStart - oldTransStart) / 1000.0;  // Segundos
+
+            // Control sesión si han transcurrido más de 2 minutos desde última transacción
+            if (offsetTrans <= 120.0) {
+                // Actualizar timestamp en sesión
+                req.session.lastTransStart = newTransStart;
+            } else {
+                // Destruir sesión si se sobrepasa timeout y redirigir a ruta anterior
+                delete req.session.user;
+                delete req.session.lastTransStart;
+
+                res.redirect(req.session.redir.toString());
+
+                return;
+            }
+        }
+    }
+
+    // Hacer visible la sesión a las vistas
+    res.locals.session = req.session;
+
+    next();
+});
 
 module.exports = app;
